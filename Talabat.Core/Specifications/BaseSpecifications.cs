9e3154dfd8a7f7bf176e541ec95775ab094e4ae3
3moJunioR <m.wa7ba@gmail.com>
2025-09-14 22:16:32 +0300

    Specification DesignPattern

--- Talabat.Core/Specifications/BaseSpecifications.cs
+++ Talabat.Core/Specifications/BaseSpecifications.cs
@@ -14,7 +14,6 @@ namespace Talabat.Core.Specifications
         public List<Expression<Func<T, object>>> Includes { get; set;  } = new List<Expression<Func<T, object>>>();
         public BaseSpecifications()
         {
-            
         }
         public BaseSpecifications(Expression<Func<T, bool>> criteria)
         {
